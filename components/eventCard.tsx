5ee1954018885731bcd2063e4301502fb903236e
Wei-en Tan <wtan235@gmail.com>
2026-01-12 13:58:33 -0500

    button for invitees

--- components/eventCard.tsx
+++ components/eventCard.tsx
@@ -1,4 +1,4 @@
-import { Pencil1Icon } from "@radix-ui/react-icons";
+import { PaperPlaneIcon, Pencil1Icon } from "@radix-ui/react-icons";
 import { Card } from "@radix-ui/themes";
 
 type cardProps = {
@@ -14,13 +14,22 @@ export default function EventCard(props: cardProps) {
         <h1>{props.name}</h1>
         <p>{props.desc}</p>
         <p>{props.date.toLocaleDateString()}</p>
-        <button className="group absolute right-2 bottom-2 h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,#4FBBC9,#8869D1]">
-          <Pencil1Icon
-            className="m-auto transition-all duration-500 group-hover:rotate-360"
-            width="32"
-            height="32"
-          />
-        </button>
+        <div className="absolute right-2 bottom-2 flex gap-2">
+          <button className="group h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,#4FBBC9,#8869D1]">
+            <PaperPlaneIcon
+              className="m-auto transition-all duration-500 group-hover:rotate-360"
+              width="24"
+              height="24"
+            />
+          </button>
+          <button className="group h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,#4FBBC9,#8869D1]">
+            <Pencil1Icon
+              className="m-auto transition-all duration-500 group-hover:rotate-360"
+              width="24"
+              height="24"
+            />
+          </button>
+        </div>
       </Card>
     </div>
   );
