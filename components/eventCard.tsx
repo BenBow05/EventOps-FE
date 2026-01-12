011c09adcce31e104e068d59facbe432433dc62f
Wei-en Tan <wtan235@gmail.com>
2026-01-12 16:28:50 -0500

    image and guests modal

--- components/eventCard.tsx
+++ components/eventCard.tsx
@@ -1,5 +1,6 @@
-import { PaperPlaneIcon, Pencil1Icon } from "@radix-ui/react-icons";
-import { Card } from "@radix-ui/themes";
+import { Pencil1Icon } from "@radix-ui/react-icons";
+import { Card, Inset } from "@radix-ui/themes";
+import ManageGuestsModal from "./manageGuestsModal";
 
 type cardProps = {
   name: string;
@@ -11,18 +12,18 @@ export default function EventCard(props: cardProps) {
   return (
     <div className="h-100 w-100">
       <Card className="h-full w-full">
-        <h1>{props.name}</h1>
+        <Inset
+          className="h-2/3 bg-linear-[25deg,var(--primary),var(--secondary)]"
+          clip="padding-box"
+          side="top"
+          pb="current"
+        />
+        <h1 className="mt-2 font-bold">{props.name}</h1>
         <p>{props.desc}</p>
         <p>{props.date.toLocaleDateString()}</p>
         <div className="absolute right-2 bottom-2 flex gap-2">
-          <button className="group h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,#4FBBC9,#8869D1]">
-            <PaperPlaneIcon
-              className="m-auto transition-all duration-500 group-hover:rotate-360"
-              width="24"
-              height="24"
-            />
-          </button>
-          <button className="group h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,#4FBBC9,#8869D1]">
+          <ManageGuestsModal />
+          <button className="group h-12 w-12 rounded-sm border-2 border-black text-6xl transition-all duration-100 hover:scale-110 hover:border-transparent hover:bg-linear-[25deg,var(--primary),var(--secondary)]">
             <Pencil1Icon
               className="m-auto transition-all duration-500 group-hover:rotate-360"
               width="24"
