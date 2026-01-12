b3906d61089bb177a83736f5fcfa31204799b339
Wei-en Tan <wtan235@gmail.com>
2026-01-12 13:58:44 -0500

    modal for creating event (non func)

--- components/manage.tsx
+++ components/manage.tsx
@@ -1,5 +1,5 @@
-import { PlusIcon } from "@radix-ui/react-icons";
 import EventCard from "./eventCard";
+import CreateEventModal from "./createEventModal";
 
 type eventProps = {
   events: { id: number; name: string; desc: string; date: Date }[];
@@ -8,13 +8,6 @@ type eventProps = {
 export default function Manage(props: eventProps) {
   return (
     <div className="m-auto">
-      <button className="group absolute right-5 bottom-5 h-16 w-16 rounded-sm bg-linear-[25deg,#4FBBC9,#8869D1] text-6xl transition-all duration-100 hover:scale-110">
-        <PlusIcon
-          className="m-auto transition-all duration-500 group-hover:rotate-180"
-          width="45"
-          height="45"
-        />
-      </button>
       <div className="m-auto grid h-fit w-fit grid-cols-3 gap-5 align-middle">
         {props.events.map((event) => (
           <EventCard
@@ -25,6 +18,7 @@ export default function Manage(props: eventProps) {
           />
         ))}
       </div>
+      <CreateEventModal />
     </div>
   );
 }
